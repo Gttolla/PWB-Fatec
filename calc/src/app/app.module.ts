2e82c581278f298c3ec8eec1038cf1c2b83576c1
Lucas Pedro Chaves <lucaspedrosupergeeks@gmail.com>
2025-03-01 00:07:08 -0300

    feat: counts history system done

--- calc/src/app/app.module.ts
+++ calc/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,7 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-      
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- calc/src/app/form/form.component.ts
+++ calc/src/app/form/form.component.ts
@@ -11,26 +11,49 @@ export class FormComponent {
   num2: number = 0;
   result: number = 0;
 
+  showHistory: boolean = false;
+  history: string[] = [];
+  currentNumbers: string = '';
+
   onClickSum() {
     this.result = this.num1 + this.num2;
+    this.history.push(`${this.num1} + ${this.num2} = ${this.result}`);
   }
   onClickSub() {
     this.result = this.num1 - this.num2;
+    this.history.push(`${this.num1} - ${this.num2} = ${this.result}`);
   }
   onClickMul() {
     this.result = this.num1 * this.num2;
+    this.history.push(`${this.num1} × ${this.num2} = ${this.result}`);
   }
-  onClickDiv() { 
+  onClickDiv() {
     this.result = this.num1 / this.num2;
+    this.history.push(`${this.num1} ÷ ${this.num2} = ${this.result}`);
   }
   onClickPow() {
     this.result = Math.pow(this.num1, this.num2);
+    this.history.push(`${this.num1} ^ ${this.num2} = ${this.result}`);
   }
   onClickSqrt() {
     this.result = Math.sqrt(this.num1);
+    this.history.push(`√${this.num1} = ${this.result}`);
   }
-  onClickClearField(filed: string) {
-    filed === 'num1' ? this.num1 = 0 : this.num2 = 0;
+  onClickClearField(field: string) {
+    field === 'num1' ? this.num1 = 0 : this.num2 = 0;
     this.result = 0;
   }
+  onClickClearHistory() {
+    this.history = [];
+    this.num1 = 0;
+    this.num2 = 0;
+    this.result = 0;
+  }
+
+  onClickAddNumbersToCount(numbers: string) {
+    this.currentNumbers = numbers;
+    this.num1 = Number(this.currentNumbers.split(' ')[0]);
+    this.num2 = Number(this.currentNumbers.split(' ')[2]);
+  }
+
 }
