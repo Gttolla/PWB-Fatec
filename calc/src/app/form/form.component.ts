012abe0b084d186d0791bc88afb4019a09e5e046
Lucas Pedro Chaves <lucaspedrosupergeeks@gmail.com>
2025-02-28 22:24:10 -0300

    feat: 6 math operations working and design redesign

--- calc/src/app/form/form.component.ts
+++ calc/src/app/form/form.component.ts
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  num1: number = 0;
+  num2: number = 0;
+  result: number = 0;
 
+  onClickSum() {
+    this.result = this.num1 + this.num2;
+  }
+  onClickSub() {
+    this.result = this.num1 - this.num2;
+  }
+  onClickMul() {
+    this.result = this.num1 * this.num2;
+  }
+  onClickDiv() { 
+    this.result = this.num1 / this.num2;
+  }
+  onClickPow() {
+    this.result = Math.pow(this.num1, this.num2);
+  }
+  onClickSqrt() {
+    this.result = Math.sqrt(this.num1);
+  }
+  onClickClearField(filed: string) {
+    filed === 'num1' ? this.num1 = 0 : this.num2 = 0;
+    this.result = 0;
+  }
 }
